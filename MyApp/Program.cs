40c286563e1663e02bd5819e6380653c857f3cfc
Arina Liubas <arinaliubas@gmail.com>
2025-03-04 14:23:21 +0100

    Modification 2

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,3 +1,4 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("This is Modification 1");
+Console.WriteLine("This is Modification 2");
