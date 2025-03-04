c8cfd882118d169703960854a0d1fd9f13c818af
Arina Liubas <arinaliubas@gmail.com>
2025-03-04 14:29:18 +0100

    Modification 3

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,4 +1,10 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
 Console.WriteLine("This is Modification 1");
 Console.WriteLine("This is Modification 2");
+
+static void NewMethod(){
+Console.WriteLine("This is a new method in Modification 3");
+}
+NewMethod();
